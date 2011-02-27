09e3d71ea5531c9e88d0843cc68ff429bbf488f6
Luca Barbato <lu_zero@gentoo.org>
2011-02-27 01:41:01 +0100

    Initial tcp-alike sctp support
    Please do not use it yet for anything serious, error handling and socket
    tracking is missing
    
    Conflicts:
    
    	libavformat/Makefile
    	libavformat/allformats.c

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -260,7 +260,7 @@ void av_register_all(void)
     REGISTER_PROTOCOL (TCP, tcp);
     REGISTER_PROTOCOL (TLS, tls);
     REGISTER_PROTOCOL (UDP, udp);
-
+    REGISTER_PROTOCOL (SCTP, sctp);
     /* external libraries */
     REGISTER_MUXDEMUX (LIBNUT, libnut);
     REGISTER_PROTOCOL (LIBRTMP, librtmp);
