9ea0bcf3322804242eabcc3aceb07296ab8fdebc
Jordi Ortiz <nenjordi@gmail.com>
2012-05-14 20:16:43 +0200

    avserv: Add basic polling

--- avserv.c
+++ avserv.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,7 +8,11 @@
 #include <stdio.h>
 
 #include "cmdutils.h"
+#include "libavformat/internal.h"
 #include "libavutil/log.h"
+#include "libavutil/random_seed.h"
+#include "libavformat/rtmp.h"
+#include "libavformat/url.h"
 
 const char program_name[] = "avserv";
 const int program_birth_year = 2012;
@@ -39,6 +44,7 @@ static void show_help(void)
 static void opt_debug(void)
 {
     avserv_options.debug = 1;
+    av_log_set_level(AV_LOG_DEBUG);
 }
 
 /* Command line options */
@@ -50,24 +56,182 @@ static const OptionDef options[] = {
     { NULL },
 };
 
+struct rtmp_info {
+    uint32_t epoch;
+    uint32_t my_epoch;
+    uint8_t peer_random[RTMP_HANDSHAKE_PACKET_SIZE - 8];
+    uint8_t my_random[RTMP_HANDSHAKE_PACKET_SIZE - 8];
+};
+
 struct avserv_thread_params {
     int socket;
+    URLContext *clientctx;
+    struct pollfd pollst;
+    struct rtmp_info rtmpctx;
+    struct sockaddr_in *clientaddr;
 };
+
+
+static int num_clients;
+
+struct client_context {
+    pthread_t *thread;
+    struct client_context* next;
+} *clients;
+
+enum rtmp_handshake_states {
+    RTMP_UNINITIALIZED_HS,
+    RTMP_VERSIONSENT_HS,
+    RTMP_ACKSENT_HS
+};
+
+static int rtmp_handshake(struct avserv_thread_params *param)
+{
+    //struct pollfd pollst;
+    uint8_t buffer[RTMP_HANDSHAKE_PACKET_SIZE];
+    int eventnum      = 0;
+//    uint32_t epoch;
+//    uint32_t my_epoch;
+    uint32_t temp;
+//    uint8_t peer_random[RTMP_HANDSHAKE_PACKET_SIZE - 8];
+//    uint8_t my_random[RTMP_HANDSHAKE_PACKET_SIZE - 8];
+    enum rtmp_handshake_states handshake_state = RTMP_UNINITIALIZED_HS;
+    int randomidx = 0;
+    uint8_t error  = 0;
+    ssize_t inoutsize = 0;
+
+    param->pollst.fd     = param->socket;
+    param->pollst.events = POLLIN | POLLPRI;
+    while ((eventnum = poll(&param->pollst, 1, -1)) > 0 && !error) {
+        av_log(NULL, AV_LOG_DEBUG, "Event[%d] %d\n", eventnum, param->pollst.revents);
+        if ((param->pollst.revents & POLLIN) || (param->pollst.revents & POLLPRI)) {
+            switch(handshake_state) {
+            case RTMP_UNINITIALIZED_HS:
+                inoutsize = read(param->socket,buffer,1); //Receive C0
+                if (inoutsize == 0) return AVERROR(EPROTO);
+                if (buffer[0] == 3) /* Check Version */
+                    if(!write(param->socket, buffer, 1))       //Send S0
+                    {
+                        av_log(NULL, AV_LOG_ERROR, "Unable to write answer - RTMP S0\n");
+                        return AVERROR(EPROTO);
+                    }
+                handshake_state = RTMP_VERSIONSENT_HS;
+                break;
+            case RTMP_VERSIONSENT_HS:
+                inoutsize = read(param->socket,buffer,4096);
+                if (inoutsize == 0) return AVERROR(EPROTO);
+                if (inoutsize != RTMP_HANDSHAKE_PACKET_SIZE)
+                {
+                    av_log(NULL, AV_LOG_ERROR, "Erroneous C1 Message size %d not following standard\n", inoutsize);
+                    return AVERROR(EPROTO);
+                }
+                memcpy(&temp, buffer, 4);
+                param->rtmpctx.epoch = ntohl(temp);
+                param->rtmpctx.my_epoch = param->rtmpctx.epoch;
+                memcpy(&temp, buffer + 4, 4);
+                temp  = ntohl(temp);
+                if (temp != 0) {
+                    av_log(NULL, AV_LOG_ERROR, "Erroneous C1 Message zero != 0 --> %d\n", temp);
+                    return AVERROR(EPROTO);
+                }
+                memcpy(param->rtmpctx.peer_random, buffer + 8, RTMP_HANDSHAKE_PACKET_SIZE - 8);
+                //Send answer
+                /*By now same epoch will be send*/
+                for (randomidx = 0; randomidx < (RTMP_HANDSHAKE_PACKET_SIZE - 8); randomidx += 4) {
+                    temp = av_get_random_seed();
+                    memcpy(param->rtmpctx.my_random + randomidx, &temp, 4);
+                }
+                memcpy(buffer + 8, param->rtmpctx.my_random, RTMP_HANDSHAKE_PACKET_SIZE - 8);
+                inoutsize = write(param->socket, buffer, RTMP_HANDSHAKE_PACKET_SIZE);
+                if (inoutsize != RTMP_HANDSHAKE_PACKET_SIZE) {
+                    av_log(NULL, AV_LOG_ERROR, "Unable to write answer - RTMP S1\n");
+                    return AVERROR(EPROTO);
+                }
+                handshake_state = RTMP_ACKSENT_HS;
+                break;
+            case RTMP_ACKSENT_HS:
+                inoutsize = read(param->socket,buffer,4096);
+                if (inoutsize == 0) return AVERROR(EPROTO);
+                if (inoutsize != RTMP_HANDSHAKE_PACKET_SIZE)
+                {
+                    av_log(NULL, AV_LOG_ERROR, "Erroneous C2 Message size %d not following standard\n", inoutsize);
+                    return AVERROR(EPROTO);
+                }
+                memcpy(&temp, buffer, 4);
+                temp = ntohl(temp);
+                if (temp != param->rtmpctx.my_epoch)
+                {
+                    av_log(NULL, AV_LOG_ERROR, "Erroneous C2 Message epoch does not match up with C1 epoch\n");
+                    return AVERROR(EPROTO);
+                }
+
+                /*TODO: Should Time2 be used to synchronise timers?*/
+                if (memcmp(buffer + 8, param->rtmpctx.my_random, RTMP_HANDSHAKE_PACKET_SIZE - 8) != 0)
+                {
+                    av_log(NULL, AV_LOG_ERROR, "Erroneous C2 Message random does not match up\n");
+                    return AVERROR(EPROTO);
+                }
+                /* Send S2 */
+                temp = htonl(param->rtmpctx.epoch);
+                memcpy(buffer, &temp , 4);
+                // TODO: Time2 missing, by now 0
+                temp = 0;
+                memcpy(buffer + 4, &temp, 4);
+                memcpy(buffer + 8, param->rtmpctx.peer_random,
+                       RTMP_HANDSHAKE_PACKET_SIZE - 8);
+                inoutsize = write(param->socket, buffer,
+                                  RTMP_HANDSHAKE_PACKET_SIZE);
+                if (inoutsize != RTMP_HANDSHAKE_PACKET_SIZE) {
+                    av_log(NULL, AV_LOG_ERROR,
+                           "Unable to write answer - RTMP S2\n");
+                    return AVERROR(EPROTO);
+                }
+
+                /* Handshake successful */
+                return 0;
+                break;
+            }
+        }
+    }
+    return error;
+
+}
+
+
 static void *avserv_thread(void *arg)
 {
-    struct inspect_thread_params *param = arg;
+    struct avserv_thread_params *param = arg;
+    int ret = 0;
+    uint8_t rbuf[4096];
+    int eventnum, inoutsize;
+
     av_log(NULL, AV_LOG_INFO, "Thread starts\n");
-    for(;;); /* Thread never ends */
+    ret = rtmp_handshake(param);
+    if (ret != 0)
+        av_log(NULL, AV_LOG_ERROR, "Problem with RTMP handshake\n");
+    while ((eventnum = poll(&param->pollst, 1, -1)) > 0) {
+        av_log(NULL, AV_LOG_DEBUG, "Event[%d] %d\n", eventnum,
+               param->pollst.revents);
+        if ((param->pollst.revents & POLLIN)
+            || (param->pollst.revents & POLLPRI)) {
+                inoutsize = read(param->socket, rbuf, 4096);
+                if (inoutsize == 0) return AVERROR(EPROTO);
+        }
+    }
+    close(param->socket);
     av_log(NULL, AV_LOG_INFO, "Thread ends\n");
+    pthread_exit(0);
     return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    int ret;
-    int listensd;
-    struct sockaddr_in serveraddr = {0};
-    //TODO: ADD pthread_t monitor
+    int ret      = 0;
+    char tcpname[500];
+    //AVFormatContext *fmtctx = avformat_alloc_context();
+    URLContext *serverctx;
+
+    //TODO: ADD pthread_t monitor -- Is it really needed?
     /* Argument parsing */
     parse_options(NULL, argc, argv, options, NULL);
 
@@ -78,41 +242,48 @@ int main(int argc, char *argv[])
     /*     exit(1); */
     /* } */
 
-    if ((listensd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
-        av_log(NULL, AV_LOG_ERROR,"Unable to open socket\n"); //TODO: NULL->Context
-        return -1;
+    num_clients = 0;
+    /* if (!fmtctx) { */
+    /*     av_log(NULL, AV_LOG_ERROR, "Error initializing AVFormatContext\n"); */
+    /*     return AVERROR(ENOMEM); */
+    /* } */
+    /* fmtctx->oformat = av_guess_format("tcp", NULL, NULL); */
+    av_register_all();
+    avformat_network_init();
+
+    ff_url_join(tcpname, sizeof(tcpname), "tcp", NULL, "localhost",
+                RTMP_DEFAULT_PORT, "?accept");
+    if (ffurl_open(&serverctx, tcpname, AVIO_FLAG_READ_WRITE,
+                   NULL, NULL)) //Warning, no IO interrupt callback set,needed?
+    {
+        av_log(NULL, AV_LOG_ERROR, "Unable to open TCP for listening\n");
+        return AVERROR(EIO);
     }
-
-    serveraddr.sin_family      = AF_UNSPEC; /* Allow IPv4 and IPv6*/
-    //serveraddr.sin_addr.s_addr = inet_addr("192.168.0.100");
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    serveraddr.sin_port        = htons(5554);
-    if ( (ret = bind(listensd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr_in))) == -1) {
-        close(listensd);
-        perror("ERROR\n");
-        av_log(NULL, AV_LOG_ERROR, "Unable to bind to address %d\n", ret);  //TODO: NULL->Context
-        exit(1);
-    }
-
-    listen(listensd, 5); //Queue size to be set, 5 for testing
-
     for (;;) {
-        struct sockaddr_in clientaddr = {0};
-        socklen_t clientaddrsize;
         pthread_t avservth;
         pthread_attr_t avservth_attr;
         struct avserv_thread_params *avservth_params = {0};
-        int clientsd = accept(listensd, (struct sockaddr *)&clientaddr, &clientaddrsize);
-        if( clientsd && clientaddrsize == sizeof(clientaddr)) {
-            if(pthread_attr_init(&avservth_attr))
-                av_log(NULL, AV_LOG_ERROR, "Unable to init thread attributes\n");
+        avservth_params = av_malloc(sizeof(struct avserv_thread_params));
+        if (!avservth_params) {
+            av_log(NULL, AV_LOG_ERROR, "Unable to allocate thread params "
+                   "memory\n");
+        }
+
+        serverctx->prot->url_accept(serverctx, &avservth_params->clientctx,
+                                    -1);
+        num_clients++;
+        clients = av_realloc(clients, sizeof(struct client_context)
+                             * num_clients);
+
+            if (pthread_attr_init(&avservth_attr))
+                av_log(NULL, AV_LOG_ERROR, "Unable to init"
+                       " thread attributes\n");
             if(!(avservth_params = av_malloc(sizeof(struct avserv_thread_params))))
-                av_log(NULL, AV_LOG_ERROR, "Unable to allocate thread params memory\n");
-            avservth_params->socket = clientsd;
+                av_log(NULL, AV_LOG_ERROR, "Unable to allocate thread"
+                       " params memory\n");
             if(pthread_create(&avservth , &avservth_attr, avserv_thread, avservth_params))
                 av_log(NULL, AV_LOG_ERROR, "Unable to create listen thread\n");
             pthread_detach(avservth); /* Detach thread */
-        }
     }
     return 0;
 }
